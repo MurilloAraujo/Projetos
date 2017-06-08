f7fd7f465a03db82bc8f53699afd26064d1165f8
MurilloAraujo <murylllo2233@gmail.com>
2017-06-08 12:41:01 -0300

    Create Create.class.php

--- Crud-php7/Create.class.php
+++ Crud-php7/Create.class.php
@@ -26,11 +26,13 @@ class Create extends Conn {
      * @param ARRAY $Dados = Informe um array atribuitivo. ( Nome Da Coluna => Valor ).
      */
     public function ExeCreate($Tabela, array $Dados) {
+        
         $this->Tabela = (string) $Tabela;
         $this->Dados = $Dados;
 
         $this->getSyntax();
         $this->Execute();
+        
     }
 
     /**
@@ -38,7 +40,9 @@ class Create extends Conn {
      * @return INT $Variavel = lastInsertId OR FALSE
      */
     public function getResult() {
+        
         return $this->Result;
+        
     }
 
     /**
@@ -48,26 +52,36 @@ class Create extends Conn {
      */
     //Obtém o PDO e Prepara a query
     private function Connect() {
+        
         $this->Conn = parent::getConn();
         $this->Create = $this->Conn->prepare($this->Create);
+        
     }
 
     //Cria a sintaxe da query para Prepared Statements
     private function getSyntax() {
+        
         $Fileds = implode(', ', array_keys($this->Dados));
         $Places = ':' . implode(', :', array_keys($this->Dados));
         $this->Create = "INSERT INTO {$this->Tabela} ({$Fileds}) VALUES ({$Places})";
+        
     }
 
     //Obtém a Conexão e a Syntax, executa a query!
     private function Execute() {
+        
         $this->Connect();
+        
         try {
+            
             $this->Create->execute($this->Dados);
             $this->Result = $this->Conn->lastInsertId();
+            
         } catch (PDOException $e) {
+            
             $this->Result = null;
             WSErro("<b>Erro ao cadastrar:</b> {$e->getMessage()}", $e->getCode());
+            
         }
     }
 
